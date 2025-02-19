be63a31cd4a99d304a481f0544ea906435d6c686
Alex Sorokin <regsomemail@gmail.com>
2025-02-19 22:21:52 +0100

    files upd

--- electron/src/window/createWindow/createWindow.ts
+++ electron/src/window/createWindow/createWindow.ts
@@ -1,18 +1,12 @@
-import { BrowserWindow, app } from 'electron';
-import { DEV_MODE } from '@e/electronConstants';
-import { isFirstRun } from '@e/firstRun/firstRun';
+import { BrowserWindow } from 'electron';
 import { getWindowProps } from '@e/window/createWindow/getWindowProps';
 import { initWindowExtras } from '@e/window/createWindow/initWindowExtras';
 import { setWindowUrl } from '@e/window/createWindow/setWindowUrl';
-import { initWindowListeners } from '@e/window/windowListeners';
 
 export let appWindow: BrowserWindow;
 
 export const createWindow = () => {
-  if (!DEV_MODE && !isFirstRun) app.dock.hide();
-
   appWindow = new BrowserWindow(getWindowProps());
-  initWindowListeners();
   initWindowExtras();
   setWindowUrl();
 };
--- electron/src/window/showWindow.ts
+++ electron/src/window/showWindow.ts
@@ -1,12 +1,8 @@
-import { blockCmdW } from '@e/globalShortcutsController';
-import { sendCreateWindowEvent } from '@e/ipc/ipcActions';
 import { appWindow, createWindow } from '@e/window/createWindow/createWindow';
 import { getWindowSizeProps } from '@e/window/createWindow/getWindowSizeProps';
 
 export const showWindow = () => {
-  blockCmdW();
   if (!appWindow) createWindow();
   appWindow.setBounds(getWindowSizeProps());
   appWindow.show();
-  sendCreateWindowEvent();
 };
