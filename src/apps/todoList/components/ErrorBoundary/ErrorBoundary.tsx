f7300bf1dbe26b6d982300b9293e5e7f7ee2252d
Alex Sorokin <regsomemail@gmail.com>
2025-02-22 13:25:37 +0100

    added ErrorBoundary

---
+++ src/apps/todoList/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,20 @@
+import { Component, ReactNode } from 'react';
+
+type Props = { children: ReactNode; fallback: ReactNode };
+type State = { hasError: boolean };
+
+export class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  render(): ReactNode {
+    if (this.state.hasError) return this.props.fallback;
+    return this.props.children;
+  }
+}
--- src/apps/todoList/components/TodoList/TodoList.tsx
+++ src/apps/todoList/components/TodoList/TodoList.tsx
@@ -1,18 +1,27 @@
 import styles from './TodoList.module.scss';
 import '@/apps/todoList/todoStyles.module.scss';
 import { Add } from '@/apps/todoList/components/Add/Add';
+import { ErrorBoundary } from '@/apps/todoList/components/ErrorBoundary/ErrorBoundary';
 import { List } from '@/apps/todoList/components/List/List';
 import { useGetTodos } from '@/apps/todoList/hooks/useGetTodos';
+
+const Fallback = () => (
+  <div className={styles.todoContainer}>
+    <div className={styles.todo}>Please reload the app</div>
+  </div>
+);
 
 export const TodoList = () => {
   useGetTodos();
 
   return (
-    <div className={styles.todoContainer}>
-      <div className={styles.todo}>
-        <Add />
-        <List />
+    <ErrorBoundary fallback={<Fallback />}>
+      <div className={styles.todoContainer}>
+        <div className={styles.todo}>
+          <Add />
+          <List />
+        </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 };
