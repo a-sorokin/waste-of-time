fe32044056e0966576afe229d402911b1e17ffa6
Alex Sorokin <regsomemail@gmail.com>
2025-02-23 00:32:05 +0100

    readme update

--- src/apps/readme/components/Readme/Readme.tsx
+++ src/apps/readme/components/Readme/Readme.tsx
@@ -4,12 +4,13 @@ export const Readme = () => {
   return (
     <div className={styles.readme}>
       <h3>Welcome to WasteOfTimeOS v0.1.8</h3>
+
       <span>What you can do</span>
+
       <li>Open apps</li>
       <li>Move</li>
       <li>Resize</li>
       <li>Enjoy the ASCII donut</li>
-      <li>Play Tetris</li>
     </div>
   );
 };
