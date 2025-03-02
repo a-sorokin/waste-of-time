4a3c2cfb0cf9a6b580f1386349afa0ced2676e0c
Alex Sorokin <regsomemail@gmail.com>
2025-03-02 16:26:21 +0100

    commented react-scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import { scan } from 'react-scan';
+// import { scan } from 'react-scan';
 import '@/styles/index.scss';
 import '@/styles/themes.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from '@/App';
-import { DEV_MODE } from '@e/constants';
 
-if (DEV_MODE) scan({ enabled: DEV_MODE });
+// scan({ enabled: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
