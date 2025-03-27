83b7ba36f1883704290d798f6b7c2cd111737265
Alex Sorokin <regsomemail@gmail.com>
2025-03-27 16:24:20 +0100

    small upd

--- src/apps/ast/logic/evaluators/evaluateFunction.ts
+++ src/apps/ast/logic/evaluators/evaluateFunction.ts
@@ -2,20 +2,20 @@ import { add } from '@ast/logic/astFunctions/add';
 import { equals } from '@ast/logic/astFunctions/equals';
 import { not } from '@ast/logic/astFunctions/not';
 import { EXPRESSION_NAMES } from '@ast/logic/constants';
-import { parseParameters } from '@ast/logic/evaluators/utils';
+import { parseOneParameter, parseTwoParameters } from '@ast/logic/evaluators/utils';
 import { Expression } from '@ast/logic/types';
 
 const evaluateFunctionActions = {
   [EXPRESSION_NAMES.add]: (expression: Expression) => {
-    const [p1, p2] = parseParameters(expression);
+    const [p1, p2] = parseTwoParameters(expression);
     return add(p1, p2);
   },
   [EXPRESSION_NAMES.equals]: (expression: Expression) => {
-    const [p1, p2] = parseParameters(expression);
+    const [p1, p2] = parseTwoParameters(expression);
     return equals(p1, p2);
   },
   [EXPRESSION_NAMES.not]: (expression: Expression) => {
-    return not(expression.parameters[0]);
+    return not(parseOneParameter(expression));
   },
 };
 
--- src/apps/ast/logic/evaluators/utils.ts
+++ src/apps/ast/logic/evaluators/utils.ts
@@ -1,7 +1,11 @@
 import { evaluate } from '@ast/logic/evaluators/evaluate';
 import { Expression } from '@ast/logic/types';
 
-export const parseParameters = (expression: Expression) => {
+export const parseOneParameter = (expression: Expression) => {
+  return evaluate(expression.parameters[0]);
+};
+
+export const parseTwoParameters = (expression: Expression) => {
   const param1 = evaluate(expression.parameters[0]);
   const param2 = evaluate(expression.parameters[1]);
   return [param1, param2];
